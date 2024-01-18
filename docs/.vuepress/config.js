4d54c96302d2055782e7ab4ff73268d256477ed7
happyntec <tec@happyn.cn>
2024-01-18 03:20:21 +0000

    set default language to zh-CN

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,14 @@ module.exports = {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     '/': {
-      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+      title: 'happyndoc',
+      description: 'VuePress模板，主题，插件演示。展示了一个简单的VuePress案例模板，用户可以直接clone这个仓库，作为初始化一个VuePress网站启动项目，在这个项目的基础上新增自定义配置和功能。'
+    },
+    '/en/': {
+      lang: 'en-US',
       title: 'happyndoc',
       description: 'VuePress template, theme, and plugin Demo. The purpose is that users can directly clone this repository as a startup project for initializing a VuePress website, and then add custom configurations and functions based on this project.'
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'VuePress模板',
-      description: 'VuePress模板，主题，插件演示。展示了一个简单的VuePress案例模板，用户可以直接clone这个仓库，作为初始化一个VuePress网站启动项目，在这个项目的基础上新增自定义配置和功能。'
     }
   },
   plugins: [
@@ -19,8 +19,8 @@ module.exports = {
     ['qrcode',{
       // "/" and "/zh/" correspond to the path set by locales
         labelText: {
-          "/": "QRCode",
-          "/zh/": "二维码",
+          "/": "二维码",
+          "/en": "QRCode",
         },
         size:'small',
         channel:true
@@ -29,7 +29,7 @@ module.exports = {
   themeConfig: {
 
     locales: {
-      '/': {
+      '/en': {
         selectText: 'Languages',
         label: 'English',
         ariaLabel: 'Languages',
@@ -47,15 +47,15 @@ module.exports = {
 					{ text: 'Github', link: 'https://github.com/openHacking/vuepress-template' }
         ],
         sidebar: {
-          '/guide/': [
+          '/en/guide/': [
             '',
             'theme',
             'plugin'
           ],
-          '/resource/': [],
+          '/en/resource/': [],
         }
       },
-      '/zh/': {
+      '/': {
         // 多语言下拉菜单的标题
         selectText: '选择语言',
         // 该语言在下拉菜单中的标签
@@ -72,17 +72,17 @@ module.exports = {
         // 当前 locale 的 algolia docsearch 选项
         algolia: {},
         nav: [
-          { text: '指南', link: '/zh/guide/', ariaLabel: '指南' },
+          { text: '指南', link: '/guide/', ariaLabel: '指南' },
           { text: '学习', link: 'https://lwebapp.com', ariaLabel: '学习' },
 					{ text: 'Github', link: 'https://github.com/openHacking/vuepress-template' }
         ],
         sidebar: {
-          '/zh/guide/': [
+          '/guide/': [
             '',
             'theme',
             'plugin'
           ],
-          '/zh/resource/': [],
+          '/resource/': [],
         }
       }
     }
---
+++ docs/en/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroText: happynet docs
+tagline: Template · Theme · Plugin
+actionText: Get Started →
+actionLink: /en/guide/
+features:
+- title: Template
+  details: Rich templates
+- title: Theme
+  details: Custom theme
+- title: Plugin
+  details: Multiple plugins
+footer: GNU Licensed | Copyright © Happyn.net
+---
